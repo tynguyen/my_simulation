18c621e190bccfcdb9dd89f697b74ec4b2ae3189
tynguyen <tynguyen.tech@gmail.com>
2014-04-26 14:05:33 +0900

    Version 2.0 with good response

--- CartDemoPlugin.cc
+++ CartDemoPlugin.cc
@@ -19,6 +19,10 @@
 #include "gazebo/physics/physics.hh"
 #include "gazebo/transport/transport.hh"
 #include "/home/tynguyen/my_plugin/CartDemoPlugin.hh"
+#include <fstream> //ofstream
+#include <math.h> //round()
+using namespace std;
+
 
 using namespace gazebo;
 GZ_REGISTER_MODEL_PLUGIN(CartDemoPlugin)
@@ -32,6 +36,14 @@ CartDemoPlugin::CartDemoPlugin()
     this->jointPositions[i] = 0;
     this->jointVelocities[i] = 0;
     this->jointMaxEfforts[i] = 100;
+    this->aeroLoad = 0.1;
+	  this->swayForce = 10;
+     
+ 	  this->maxSpeed = 10;
+  	this->frontPower = 50;
+ 		this->rearPower = 50;
+    //Max wheel angle
+    this->wheelRadius = 0.2;
   }
 }
 
@@ -66,11 +78,11 @@ void CartDemoPlugin::Load(physics::ModelPtr _model,
   this->joints[1] = _model->GetJoint(
     _sdf->GetElement("right")->Get<std::string>());
   this->jointPIDs[1] = common::PID(
-    _sdf->GetElement("right_pid")->Get<math::Vector3>().x,
-    _sdf->GetElement("right_pid")->Get<math::Vector3>().y,
-    _sdf->GetElement("right_pid")->Get<math::Vector3>().z,
-    _sdf->GetElement("right_ilim")->Get<math::Vector2d>().y,
-    _sdf->GetElement("right_ilim")->Get<math::Vector2d>().x);
+    _sdf->GetElement("gas")->Get<math::Vector3>().x,
+    _sdf->GetElement("gas")->Get<math::Vector3>().y,
+    _sdf->GetElement("gas")->Get<math::Vector3>().z,
+    _sdf->GetElement("gas_ilim")->Get<math::Vector2d>().y,
+    _sdf->GetElement("gas_ilim")->Get<math::Vector2d>().x);
   this->jointPositions[1] =
     _sdf->GetElement("right_pos")->Get<double>();
   this->jointVelocities[1] =
@@ -81,18 +93,28 @@ void CartDemoPlugin::Load(physics::ModelPtr _model,
   this->joints[2] = _model->GetJoint(
     _sdf->GetElement("left")->Get<std::string>());
   this->jointPIDs[2] = common::PID(
-    _sdf->GetElement("left_pid")->Get<math::Vector3>().x,
-    _sdf->GetElement("left_pid")->Get<math::Vector3>().y,
-    _sdf->GetElement("left_pid")->Get<math::Vector3>().z,
-    _sdf->GetElement("left_ilim")->Get<math::Vector2d>().y,
-    _sdf->GetElement("left_ilim")->Get<math::Vector2d>().x);
+    _sdf->GetElement("brake")->Get<math::Vector3>().x,
+    _sdf->GetElement("brake")->Get<math::Vector3>().y,
+    _sdf->GetElement("brake")->Get<math::Vector3>().z,
+    _sdf->GetElement("brake_ilim")->Get<math::Vector2d>().y,
+    _sdf->GetElement("brake_ilim")->Get<math::Vector2d>().x);
   this->jointPositions[2] =
     _sdf->GetElement("left_pos")->Get<double>();
   this->jointVelocities[2] =
     _sdf->GetElement("left_vel")->Get<double>();
   this->jointMaxEfforts[2] =
     _sdf->GetElement("left_eff")->Get<double>();
-
+  this->gasJoint = this->model->GetJoint(_sdf->Get<std::string>("gas"));
+  this->brakeJoint = this->model->GetJoint(_sdf->Get<std::string>("brake"));
+  
+  this->maxSpeed = _sdf->Get<double>("max_speed");
+  this->aeroLoad = _sdf->Get<double>("aero_load");
+  this->tireAngleRange = _sdf->Get<double>("tire_angle_range");
+  this->frontPower = _sdf->Get<double>("front_power");
+  this->rearPower = _sdf->Get<double>("rear_power");
+  
+  
+  
   this->updateConnection = event::Events::ConnectWorldUpdateBegin(
           boost::bind(&CartDemoPlugin::OnUpdate, this));
 }
@@ -102,6 +124,25 @@ void CartDemoPlugin::Init()
 {
   // physics::EntityPtr parent = boost::dynamic_pointer_cast<physics::Entity>(
   //   this->joints[0]->GetChild());
+  // The total range the steering wheel can rotate
+  double steeringRange = this->joints[0]->GetHighStop(0).Radian() -
+                         this->joints[0]->GetLowStop(0).Radian();
+  // Compute the angle ratio between the steering wheel and the tires
+  this->steeringRatio = steeringRange / this->tireAngleRange;
+  // Maximum gas is the upper limit of the gas joint
+  this->maxGas = this->gasJoint->GetHighStop(0).Radian();
+
+  // Maximum brake is the upper limit of the gas joint
+  this->maxBrake = this->gasJoint->GetHighStop(0).Radian();
+  //   A little force to push back on the pedals
+  this->gasJoint->SetForce(0, -0.1);
+  this->brakeJoint->SetForce(0, -0.1);
+
+  printf("SteeringRation[%f] MaxGa[%f]\n", this->steeringRatio, this->maxGas);
+  
+  //Create a file to ouput parameters
+  ofstream myfile;
+  myfile.open ("pidOut.csv", ios::out| ios::trunc); 
 }
 
 /////////////////////////////////////////////////
@@ -110,55 +151,134 @@ void CartDemoPlugin::OnUpdate()
   common::Time currTime = this->model->GetWorld()->GetSimTime();
   common::Time stepTime = currTime - this->prevUpdateTime;
   this->prevUpdateTime = currTime;
+  
+  // Get the normalized gas and brake amount
+  double gas = this->gasJoint->GetAngle(0).Radian() / this->maxGas;
+  double brake = this->brakeJoint->GetAngle(0).Radian() / this->maxBrake;
 
+
+
+  // Get the steering angle
+  double steeringAngle = this->joints[0]->GetAngle(0).Radian();
+
+  // Compute the angle of the front wheels.
+  double wheelAngle = steeringAngle / this->steeringRatio;
+
+  // double idleSpeed = 0.5;
+
+  
+  
+  
   for (int i = 0; i < 1; i++)
   {
     // first joint, set position
     double pos_target = this->jointPositions[i];
     double pos_curr = this->joints[i]->GetAngle(0).Radian();
     double max_cmd = this->jointMaxEfforts[i];
-
+    double steerVel = 0;
     double pos_err = pos_curr - pos_target;
 
     double effort_cmd = this->jointPIDs[i].Update(pos_err, stepTime);
     effort_cmd = effort_cmd > max_cmd ? max_cmd :
       (effort_cmd < -max_cmd ? -max_cmd : effort_cmd);
     this->joints[i]->SetForce(0, effort_cmd);
-    gzdbg << "steer [" << pos_curr << "] [" << pos_target << "]";
+    gzdbg << "steer [" << pos_curr << "] [" << pos_target << "]"<<"steer_effort:"<<effort_cmd;
+//    this->joints[i]->SetVelocity(1, steerVel);
   }
-/*Forward => backward => stop*/
-    for (int i = 1; i < NUM_JOINTS; i++)
-  {
-    double tmp_t = currTime.Double();
-    double eff;
-    // custom test
-    if (tmp_t < 10)      eff = 0;
-    else if (tmp_t < 20) eff = this->jointMaxEfforts[i];
-    else if (tmp_t < 30) eff = -this->jointMaxEfforts[i];
-    else if (tmp_t < 40) eff = -this->jointMaxEfforts[i];
-    else if (tmp_t < 50) eff = this->jointMaxEfforts[i];
-    else 
-    {
-      /* pid to velocity */
-      double vel_target = this->jointVelocities[i];
-      double vel_curr = this->joints[i]->GetVelocity(0);
-//      double max_cmd = 100.0;  
-      double max_cmd = this->jointMaxEfforts[i];
-
-      double vel_err = vel_curr - vel_target;
-
-      eff = this->jointPIDs[i].Update(vel_err, stepTime);
-      eff = eff > max_cmd ? max_cmd :
-        (eff < -max_cmd ? -max_cmd : eff);
-    }
+  
     
-//    else if (tmp_t < 40) eff = -this->jointMaxEfforts[i];
-//    else if (tmp_t < 50) eff = this->jointMaxEfforts[i];
-//    else if (tmp_t < 60)
-//    {
-//      /* pid to velocity */
-//      double vel_target = this->jointVelocities[i];
-//      double vel_curr = this->joints[i]->GetVelocity(0);
+/*Forward => backward => stop*/
+    double tmp_t = currTime.Double();
+    double vel_target;
+    
+    // custom test
+    
+    
+    
+    
+    /* Pid to velocity */
+    if (tmp_t < 10)      
+      vel_target = 0;
+    else if (tmp_t<20)
+    	vel_target = 1.0;
+    else if (tmp_t<30)
+    	vel_target = 2.0;
+    	
+    else if (tmp_t<40)
+    	vel_target = 5.0;
+    else if (tmp_t<50)
+    	vel_target = 6.0;
+    else if (tmp_t<60)
+    	vel_target = 8.0;
+    else if (tmp_t<90)
+    	vel_target = 10.0;
+    else if (tmp_t<100)
+    	vel_target = 9.0;
+    else if (tmp_t<110)
+    	vel_target = 7.0;
+    else if (tmp_t<120)
+    	vel_target = 4.0;
+    else if (tmp_t<140)
+    	vel_target = 2.0;
+    else if (tmp_t<150)
+    	vel_target = 0.0;
+    else vel_target = 0.0;
+    double vel_curr = this->joints[1]->GetVelocity(0);
+    double vel_err = vel_curr - vel_target;
+    double gas_err = vel_err*this->wheelRadius/maxSpeed;
+    double brake_err = -vel_err*this->wheelRadius/maxSpeed;
+    double max_cmd = 0.2;
+    
+    //Why with vel_target=1, brake = gas = 0.3678? while with target 2, vehicle move with gas and brake = zero
+   	//jointVel is always zeros, 
+   
+	  if(vel_err >= 0.2)
+	  {
+	    gas = 0;
+	    brake = this->jointPIDs[2].Update(brake_err, stepTime) + 0.05;
+	    brake = brake > max_cmd ? max_cmd :
+        (brake < -max_cmd ? -max_cmd : brake);
+	  }
+	  if(vel_err >= 0.02 && vel_err < 0.1)
+	  {
+	    gas = 0;
+	    brake = this->jointPIDs[2].Update(brake_err, stepTime) + 0.01;
+	    brake = brake > max_cmd ? max_cmd :
+        (brake < -max_cmd ? -max_cmd : brake);
+	  }
+	  
+	  if(vel_err >= 0 && vel_err < 0.02)
+	  {
+	    gas = 0;
+	    brake = this->jointPIDs[2].Update(brake_err, stepTime) + 0.002;
+	    brake = brake > max_cmd ? max_cmd :
+        (brake < -max_cmd ? -max_cmd : brake);
+	  }
+	  
+	  if(vel_err >= 0.1 && vel_err < 0.2)
+	  {
+	  	gas = 0;
+	    brake = this->jointPIDs[2].Update(brake_err, stepTime) + 0.02;
+	    brake = brake > max_cmd ? max_cmd :
+        (brake < -max_cmd ? -max_cmd : brake);
+	  }
+	  if (vel_err < -0.01)
+	  {
+	    brake = 0;
+	  	gas = this->jointPIDs[1].Update(gas_err, stepTime) +vel_target*this->wheelRadius/maxSpeed ;
+	  	gas = gas > max_cmd ? max_cmd :
+        (gas < -max_cmd ? -max_cmd : gas);
+    }  
+    
+    
+        
+    // Compute the rotational velocity of the wheels
+	  double jointVel = (std::max(0.0, gas-brake) * this->maxSpeed) /
+                    this->wheelRadius;
+//    }   
+//      eff = eff > max_cmd ? max_cmd :
+//        (eff < -max_cmd ? -max_cmd : eff);
+  
 //      double max_cmd = 100.0;  // this->jointMaxEfforts[i];
 
 //      double vel_err = vel_curr - vel_target;
@@ -224,12 +344,27 @@ void CartDemoPlugin::OnUpdate()
 //      // gzdbg << "wheel pos [" << pos_curr << "] tar [" << pos_target << "]\n";
 //    }
 
+    for (int i = 1; i < NUM_JOINTS; i++)
+    {
     gzdbg << " wheel pos ["
           << this->joints[i]->GetAngle(0).Radian()
           << "] vel ["
-          << this->joints[i]->GetVelocity(0)
-          << "] effort [" << eff << "]";
-    this->joints[i]->SetForce(0, eff);
-  }
+          << this->joints[i]->GetVelocity(0)<<"] JointVel"<<jointVel<<"] maxSpeed ["<<maxSpeed<<"] wheelRadius["<<wheelRadius<<"] Gas, brake [{"<<gas<<"}{"<<brake<<"}]"<<"vel_target["<<vel_target;
+    
+    this->joints[i]->SetVelocity(1, jointVel);
+    this->joints[i]->SetMaxForce(1, (gas + brake) * this->rearPower);
+//this->joints[i]->SetMaxForce(1, 0.5); // with this value, good figure
+    }
   gzdbg << "\n";
+ 
+  ////////////////////////////////////////////////
+	/* Out put parameters to file "pidOut.csv"*/
+  if(tmp_t*10 == round(tmp_t*10) ) //sample each 0.1 second
+  {
+  	ofstream myfile;
+  	myfile.open ("pidOut.csv", ios::out| ios::app);  //Append to existing file
+  	myfile << tmp_t<<"\tvel\t"<< this->joints[1]->GetVelocity(0)<<"\tJointVel\t"<<jointVel
+  		<<"\tGas\t"<<gas<<"\tbrake\t"<<brake<<"\tvel_target\t"<<vel_target<<"\n";
+ 	}
 }
+
